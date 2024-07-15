cca010d0d14de59205e8f8954f7fca7afbab377d
Snapchik <snapikk@gmail.com>
2024-07-15 15:15:47 +0300

    cleared notebook from old functions, added PyTorch model for logistic regression

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import re
 import string
 import numpy as np
 import nltk
+import torch
 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
@@ -162,6 +163,36 @@ def extract_features(text, freqs):
 
     #bias term is set to 1
     x[0,0] = 1
+
+    # loop through each word in the list of words
+    for word in text:
+
+        # increment the word count for the hamas label 1
+        x[0,1] += freqs.get((word, 1), 0)
+
+        # increment the word count for the idf label 0
+        x[0,2] += freqs.get((word, 0), 0)
+
+
+    assert(x.shape == (1, 3))
+    return x
+
+def extract_features_t(text, freqs):
+    '''
+    Input:
+        tweet: a list of words for one tweet
+        freqs: a dictionary corresponding to the frequencies of each tuple (word, label)
+    Output:
+        x: a feature vector of dimension (1,3)
+    '''
+
+    # 3 elements in the form of a 1 x 3 vector
+    x = torch.zeros((1, 3))
+
+    #bias term is set to 1
+    x[0,0] = 1
+
+
 
     # loop through each word in the list of words
     for word in text:
